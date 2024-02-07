0dd259a104cb6b30a9bf1b2f0a0f27be8d9e3d7a
karim ahmadi <karimahmadi9900p@gmail.com>
2024-02-07 17:40:49 +0330

    app and index

--- app.py
+++ app.py
@@ -6,3 +6,9 @@ while feleg:
         feleg = False
     else:
         print("hello " + y)
+
+
+class Hello:
+    def __init__(self, name,l_name):
+        self.name = name
+        self.l_name = l_name
