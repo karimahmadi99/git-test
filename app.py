66a180f9372a6f50f6de86b632b182eea4e4c685
karim ahmadi <karimahmadi9900p@gmail.com>
2024-02-09 18:06:35 +0330

    hello tew

--- app.py
+++ app.py
@@ -12,3 +12,13 @@ class Hello:
     def __init__(self, name,l_name):
         self.name = name
         self.l_name = l_name
+
+
+
+n = 22
+e = 0
+while True:
+    e +=1
+    print(e)
+    if e == n :
+        break
